ee88104617324c442f167559352c20fbdef430fd
Omar Juma <jumao2669@gmail.com>
2024-01-22 16:22:27 +0300

    feat: I have implemented the soln to check for even and odd nos

---
+++ warm up/EvenOrOdd.py
@@ -0,0 +1,9 @@
+def Even_or_odd(num):
+    if num % 2 == 0:
+        return True
+    else:
+        return False
+print(Even_or_odd(10))
+print(Even_or_odd(21))
+print(Even_or_odd(101))
+print(Even_or_odd(200))
